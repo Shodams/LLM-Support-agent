dba1060a44d4c360254322dcfa9098835cea2554
Shodams <52756315+Shodams@users.noreply.github.com>
2026-05-06 18:04:25 -0400

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo "Creating virtual environment..."
+python3 -m venv venv
+
+echo "Activating environment..."
+source venv/bin/activate
+
+echo "Installing dependencies..."
+pip install --upgrade pip
+pip install -r requirements.txt
+
+echo "Setup complete!"
